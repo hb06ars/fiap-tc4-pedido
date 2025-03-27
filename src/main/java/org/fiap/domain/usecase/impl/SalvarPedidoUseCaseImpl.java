b1128daf68d59b2d09518fb5774154fc5312f827
SISGDSOLAR <32486795+hb06ars@users.noreply.github.com>
2025-03-27 10:19:11 -0300

    Ajustes
    Ajustes

--- src/main/java/org/fiap/domain/usecase/impl/SalvarPedidoUseCaseImpl.java
+++ src/main/java/org/fiap/domain/usecase/impl/SalvarPedidoUseCaseImpl.java
@@ -2,15 +2,18 @@ package org.fiap.domain.usecase.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.fiap.domain.dto.PedidoDTO;
+import org.fiap.domain.entity.ItensPedidoEntity;
 import org.fiap.domain.entity.PedidoEntity;
 import org.fiap.domain.mapper.PedidoMapper;
 import org.fiap.domain.usecase.SalvarPedidoUseCase;
 import org.fiap.infra.repository.ItensPedidoRepository;
 import org.fiap.infra.repository.PedidoRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Slf4j
+@Transactional
 public class SalvarPedidoUseCaseImpl implements SalvarPedidoUseCase {
 
     private final PedidoRepository pedidoRepository;
@@ -27,6 +30,24 @@ public class SalvarPedidoUseCaseImpl implements SalvarPedidoUseCase {
     public PedidoDTO execute(PedidoDTO pedidoDTO) {
         PedidoEntity pedidoEntity = pedidoMapper.convertEntity(pedidoDTO);
         var result = pedidoRepository.save(pedidoEntity);
-        return new PedidoDTO(result);
+        if (result.getId() != null) {
+            pedidoDTO.setId(result.getId());
+            pedidoDTO.getItensPedidoList().forEach(item -> {
+                var itemEntity = itensPedidoRepository.save(ItensPedidoEntity.builder()
+                        .pedidoId(item.getPedidoId())
+                        .skuProduto(item.getSkuProduto())
+                        .quantidade(item.getQuantidade())
+                        .dtAtualizacao(item.getDtAtualizacao())
+                        .build());
+
+                item.setId(itemEntity.getId());
+                item.setPedidoId(itemEntity.getPedidoId());
+                item.setSkuProduto(itemEntity.getSkuProduto());
+                item.setQuantidade(itemEntity.getQuantidade());
+                item.setDtAtualizacao(itemEntity.getDtAtualizacao());
+            });
+
+        }
+        return pedidoDTO;
     }
 }
