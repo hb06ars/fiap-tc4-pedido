594110b571fc50d77d26ff5b18a0c28a802be30b
SISGDSOLAR <32486795+hb06ars@users.noreply.github.com>
2025-03-27 16:18:23 -0300

    a
    a

--- src/main/java/org/fiap/app/rest/controller/Controller.java
+++ src/main/java/org/fiap/app/rest/controller/Controller.java
@@ -4,11 +4,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.fiap.app.service.postgres.PedidoService;
+import org.fiap.domain.dto.PagamentoDTO;
 import org.fiap.domain.dto.PedidoDTO;
+import org.fiap.domain.usecase.CancelarBaixaEstoqueUseCase;
 import org.fiap.domain.util.HttpStatusCodes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +23,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     private final PedidoService pedidoService;
+    private final CancelarBaixaEstoqueUseCase cancelarBaixaEstoqueUseCase;
 
-    public Controller(PedidoService pedidoService) {
+    public Controller(PedidoService pedidoService, CancelarBaixaEstoqueUseCase cancelarBaixaEstoqueUseCase) {
         this.pedidoService = pedidoService;
+        this.cancelarBaixaEstoqueUseCase = cancelarBaixaEstoqueUseCase;
     }
 
     @Operation(summary = "Buscar Pedido",
@@ -31,6 +37,15 @@ public class Controller {
     public ResponseEntity<PedidoDTO> buscarPedido(@PathVariable(name = "id") Long id) {
         log.info("requisição para efetuar pagamento foi efetuada");
         return ResponseEntity.ok(pedidoService.findById(id));
+    }
+
+    @Operation(summary = "RollBack do Estoque",
+            description = "RollBack do Estoque")
+    @ApiResponse(responseCode = HttpStatusCodes.OK, description = "Buscar Pedido.")
+    @PostMapping("/rollback")
+    public void rollBackEstoque(@RequestBody PagamentoDTO pagamentoDTO) {
+        log.info("requisição para efetuar rollback do estoque.");
+        cancelarBaixaEstoqueUseCase.execute(pagamentoDTO);
     }
 
 }
