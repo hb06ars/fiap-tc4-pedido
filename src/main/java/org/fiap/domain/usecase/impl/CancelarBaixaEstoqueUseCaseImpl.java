c4a554d09bd1741eb17308840b981867fec9a68e
SISGDSOLAR <32486795+hb06ars@users.noreply.github.com>
2025-03-27 13:37:21 -0300

    a
    a

--- src/main/java/org/fiap/domain/usecase/impl/CancelarBaixaEstoqueUseCaseImpl.java
+++ src/main/java/org/fiap/domain/usecase/impl/CancelarBaixaEstoqueUseCaseImpl.java
@@ -2,15 +2,28 @@ package org.fiap.domain.usecase.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.fiap.domain.dto.PagamentoDTO;
+import org.fiap.domain.entity.ItensPedidoEntity;
+import org.fiap.domain.entity.PedidoEntity;
 import org.fiap.domain.usecase.CancelarBaixaEstoqueUseCase;
+import org.fiap.infra.repository.ItensPedidoRepository;
+import org.fiap.infra.repository.PedidoRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Slf4j
 public class CancelarBaixaEstoqueUseCaseImpl implements CancelarBaixaEstoqueUseCase {
 
+    private final ItensPedidoRepository itensPedidoRepository;
+
+    public CancelarBaixaEstoqueUseCaseImpl(ItensPedidoRepository itensPedidoRepository) {
+        this.itensPedidoRepository = itensPedidoRepository;
+    }
+
     @Override
     public void execute(PagamentoDTO pagamentoDTO) {
+        List<ItensPedidoEntity> itensPedido = itensPedidoRepository.findByPedidoId(pagamentoDTO.getPedidoId());
         log.error("Efetuando rollback do Estoque");
     }
 }
--- src/main/java/org/fiap/infra/repository/ItensPedidoRepository.java
+++ src/main/java/org/fiap/infra/repository/ItensPedidoRepository.java
@@ -3,5 +3,9 @@ package org.fiap.infra.repository;
 import org.fiap.domain.entity.ItensPedidoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItensPedidoRepository extends JpaRepository<ItensPedidoEntity, Long> {
+
+    List<ItensPedidoEntity> findByPedidoId(Long pedidoId);
 }
